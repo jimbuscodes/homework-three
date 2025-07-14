ef9c8aab914c5a86b45c818be51720ae0a1b6224
jimbusCodes <jimbuscodes@gmail.com>
2025-07-14 14:23:02 -0400

    coded the interest calculating algorithm and adjusted css

--- app/app.js
+++ app/app.js
@@ -1,1 +1,18 @@
+let calculateBtnElement = document.getElementById("calculateBtn");
 
+calculateBtnElement.addEventListener("click", function () {
+  let principle = parseFloat(document.getElementById("principleBox").value);
+  let rate = parseFloat(document.getElementById("rateBox").value);
+  let time = parseFloat(document.getElementById("timeBox").value);
+
+  let rateDecimal = rate / 100;
+  let totalInterest = principle * (1 + rateDecimal * time);
+  let grandTotal = principle + totalInterest;
+
+  let principleOutput = principle.toFixed(2);
+  let totalInterestOutput = totalInterest.toFixed(2);
+  let grandTotalOutput = grandTotal.toFixed(2);
+
+  let resultsElement = document.getElementById("results");
+  resultsElement.textContent = `With a beginning principle of $${principleOutput} and with a growth rate of ${rate}% for ${time} years, your total interest will be $${totalInterestOutput} with a grand total of $${grandTotalOutput}.`;
+});
